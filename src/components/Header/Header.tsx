cf261bc28f2f92043632332e6b8b1de9c7effc42
Eldar Guseynov <guseynoveldar3@gmail.com>
2024-04-09 15:32:58 +1000

    remove trash

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,9 @@ import { FC, ReactElement } from "react";
 import styles from './Header.module.scss';
 import { ChangeLanguage } from "../ChangeLanguage/ChangeLanguage";
 import { NavBar } from "../NavBar/NavBar";
-import { useTranslation } from "react-i18next";
 
 
 export const Header: FC = (): ReactElement => {
-
-
-
     return (
         <header className={styles.header}>
             <div className={styles.title}>
